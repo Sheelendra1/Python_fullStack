dda0945c1c6ce39fb6ca16d8e7d3a8778925c62f
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-01-24 13:18:19 +0530

    24 jan

---
+++ function.py
@@ -0,0 +1,54 @@
+# def func():
+#     print("Hello i am in function")
+#     message = "hi there"
+#     for i in range(1, 5):
+#         print(message)
+#     print("End of function")
+
+# func()
+# -------------------------------------------
+
+# def func(name, marks):
+#     print("Hello i am in function")
+#     print(name)
+#     print(marks)
+
+#     print("End of function")
+
+# func("Sheelendra", 25)
+
+# ------------------------------------------------
+
+# def add(a,b):
+#     c=a+b
+#     d=a*b
+#     print(c)
+#     print(d)
+# add(2,3)
+
+# ----------------------------------------------------
+
+# def func(marks):
+#     sum=0
+#     for i in range(0, len(marks)):
+#         if(marks[i]%2 == 0):
+#             sum = sum+marks[i]
+    
+#     return sum
+
+
+# marks = [1, 2, 3, 4, 5]
+
+# print(func(marks))
+
+
+# ------------------------------------
+
+# def build_profile(**things):
+#     print("Profile")
+#     for i, j in things.items():
+#         print(i,j)
+
+# build_profile(name = "Sheelendra", city = "Mathura", country="india")
+
+
