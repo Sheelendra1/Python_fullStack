4c7bf3db06db46f08a3d356c921961dbf8cfdcd3
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-01-27 13:49:46 +0530

    self-> current instance

--- class.py
+++ class.py
@@ -1,4 +1,19 @@
-class Students:
-    pass
-student1 = Students()
-print(student1)
+# class Students:
+#     name = "Sheelendra"
+#     grade = 90
+#     pass
+# student1 = Students()
+# print(student1.name)
+# print(student1.grade)
+# student2 = Students()
+# print(student2.name)
+# print(student2.grade)
+
+class Person:
+  def __init__(self, name, age):
+    self.name=name
+    self.age = age
+
+p1 = Person("Sheelendra", 19)
+print(p1.name)
+print(p1.age)
