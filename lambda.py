190e2fe9d7c70e98771dcbf6fe402f7bc4124773
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-01-28 14:21:35 +0530

    oops

--- lambda.py
+++ lambda.py
@@ -9,5 +9,26 @@
 # x=lambda a,b:a+b
 # print(x(2,3))
 
-max = lambda a, b: a if a > b else b
-print(max(2,3))
+# max = lambda a, b: a if a > b else b
+# print(max(2,3))
+
+
+list1 = [1,2,3,4,5]
+res=[]
+for i in list1:
+    res.append((i*i))
+print(res)
+
+
+z=list(map(lambda x: x**2,list1))
+print(z)
+
+z=list(filter(lambda x: x%2==0,list1))
+print(z)
+
+from functools import reduce
+z = reduce(lambda x, y:x+y,list1)
+print(z)
+
+f=lambda x: x+3
+print(f(5))
