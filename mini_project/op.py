10fc539f8b3e097479cfce9bd91186220bb5d957
Sheelendra1 <sheelendrasingh1904@gmail.com>
2026-02-05 14:10:29 +0530

    mini project

--- mini_project/op.py
+++ mini_project/op.py
@@ -1,43 +1,47 @@
-from main import DbConnector
+from main import AdmissionDB
 
-db = DbConnector()
-
+db = AdmissionDB()
 while True:
-    print("1. Add Student")
-    print("2. Apply for Admission")
-    print("3. View Applications")
-    print("4. Approve Application")
-    print("5. Reject Application")
-    print("6. Exit")
+    print("1. Add Student \n")
+    print("2. Apply for Admission\n")
+    print("3. View Applications\n")
+    print("4. Approve Application\n")
+    print("5. Reject Application\n")
+    print("6. Exit\n")
 
-    choice = input("Enter your choice: ")
+
+    choice = input("Enter choice: ")
 
     if choice == "1":
-        name = input("Enter Name: ")
-        email = input("Enter Email: ")
-        phone = int(input("Enter Phone: "))
-        city = input("Enter City: ")
+        name = input("Name: ")
+        email = input("Email: ")
+        phone = input("Phone: ")
+        city = input("City: ")
         db.add_student(name, email, phone, city)
 
     elif choice == "2":
-        student_id = int(input("Enter Student ID: "))
-        course = input("Enter Course: ")
-        marks = int(input("Enter Marks: "))
+        student_id = int(input("Student ID: "))
+        course = input("Course: ")
+        marks = int(input("Marks: "))
         db.apply_for_admission(student_id, course, marks)
 
     elif choice == "3":
         db.view_all_applications()
 
     elif choice == "4":
-        app_id = int(input("Enter Application ID: "))
+        app_id = int(input("Application ID: "))
         db.update_status(app_id, "Approved")
 
     elif choice == "5":
-        app_id = int(input("Enter Application ID: "))
+        app_id = int(input("Application ID: "))
         db.update_status(app_id, "Rejected")
 
     elif choice == "6":
+        db.close()
+        print("Exiting...")
         break
 
     else:
         print("Invalid choice")
+
+# db = AdmissionDB()
